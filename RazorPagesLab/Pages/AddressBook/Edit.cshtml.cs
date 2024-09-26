18c18dcb385bc350c3f11bdc6358d7d307278c91
Kevin Lammers <kevlamm21@gmail.com>
2024-09-26 13:48:33 -0400

    Updating Edit.cshtml.cs onGet & onPost scripts to connect to AddressBook entry, set UpdateRequest Fields & use mediator to send "command" to update entry & redirect to index list

--- RazorPagesLab/Pages/AddressBook/Edit.cshtml.cs
+++ RazorPagesLab/Pages/AddressBook/Edit.cshtml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -22,11 +26,33 @@ public class EditModel : PageModel
 	public void OnGet(Guid id)
 	{
 		// Todo: Use repo to get address book entry, set UpdateAddressRequest fields.
+		
+		// 1. Get the adress book entry from the repo
+		var existingAddress = _repo.Find(new EntryByIdSpecification(id)).FirstOrDefault();
+
+		// 2. Set the UpdateAddressRequest fields (ID, Line1, Line2, City, State, Zip)
+		if(existingAddress != null) //first check to make sure the ID exists if not return from the ONGet method
+		{
+			//create a new UpdateAddressRequest & list out the values
+			UpdateAddressRequest = new UpdateAddressRequest
+			{
+				Id = existingAddress.Id,
+				Line1 = existingAddress.Line1,
+				Line2 = existingAddress.Line2,
+				City = existingAddress.City,
+				PostalCode = existingAddress.PostalCode,
+				State = existingAddress.State
+			};
+		}
 	}
 
-	public ActionResult OnPost()
-	{
-		// Todo: Use mediator to send a "command" to update the address book entry, redirect to entry list.
-		return Page();
+    public async Task<ActionResult> OnPost() //updated to use async Task<> like the Create.cshtml.cs OnPost
+    {
+        if (ModelState.IsValid)
+        {
+            _ = await _mediator.Send(UpdateAddressRequest); //Use the UpdateAddress Request instead of the CreateAddressRequest 
+            return RedirectToPage("Index");
+        }
+        return Page();
 	}
 }
